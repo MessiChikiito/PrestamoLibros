6fa8cd92abc8b905fe549f2ac4fc1975ab50c42c
PaulaRubio25 <139874880+PaulaRubio25@users.noreply.github.com>
2023-12-11 23:10:54 -0500

    wewewe
    ewewewe

--- src/main/java/co/edu/ue/ui/z7Re_AutorLibro.java
+++ src/main/java/co/edu/ue/ui/z7Re_AutorLibro.java
@@ -19,10 +19,7 @@ import javax.swing.JComboBox;
  * @author mxmol
  */
 public class z7Re_AutorLibro extends javax.swing.JPanel {
-<<<<<<< HEAD
 
-=======
->>>>>>> 1268e5a1ebc9a8e5992782fa8afd54c82ed48a9f
 
     /**
      * Creates new form z3Re_Lector
